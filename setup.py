fc12580da9f6cd8afdf2c00772dc4d6920c518b6
Finn Krein <finnkrein@googlemail.com>
2020-03-27 19:33:55 +0100

    Trying to get install with pip to work

--- setup.py
+++ setup.py
@@ -6,6 +6,5 @@ setup(
     py_modules=['matrix_synapse_rest_auth_provider'],
     description="Rest auth provider for Matrix Synapse",
     include_package_data=False,
-    zip_safe=True,
     install_requires=["twisted", "requests"],
 )
